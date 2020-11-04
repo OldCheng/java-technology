1bd99f08d154a94a7e0d4a88b9442711e7ced1da
程杰 <oldchengsss@163.com>
2020-11-04 19:46:29 +0800

    hello world

---
+++ practice/src/main/com/introduction/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package main.com.introduction.test;
+
+/**
+ * @Description
+ * @Author 程杰
+ * @Date 2020/11/4 19:38
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world--!!!!!");
+    }
+}
